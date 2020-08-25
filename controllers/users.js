60590b7078d0701e2d711c14f4e4535097eaf36e
AKeyNo <arielaquino.cs@gmail.com>
2020-08-24 17:18:21 -0700

    usersRouter get request added

--- controllers/users.js
+++ controllers/users.js
@@ -2,23 +2,26 @@ const bcrypt = require("bcryptjs");
 const usersRouter = require("express").Router();
 const User = require("../models/User");
 
-usersRouter.post("/", async(request, response) => {
-    const body = request.body;
+usersRouter.post("/", async (request, response) => {
+  const body = request.body;
 
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-    const user = new User({
-        username: body.username,
-        name: body.name,
-        passwordHash
-    })
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  });
 
-    const savedUser = await user.save();
+  const savedUser = await user.save();
 
-    response.json(savedUser);
-})
+  response.json(savedUser);
+});
 
-
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users.map((user) => user.toJSON()))
+});
 
 module.exports = usersRouter;
